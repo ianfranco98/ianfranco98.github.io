b5fc6867c615df8c0951a55ea92b66862c736f0a
Ian Laurente <ian.f.laurente@gmail.com>
2025-06-17 14:35:12 -0300

    First commit

---
+++ src/data/user.js
@@ -0,0 +1,93 @@
+const INFO = {
+	main: {
+		title: "WIP Portfolio",
+		name: "Ian Laurente",
+		email: "mail@tharindu.dev",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Unreal Developer / Generalist Game Programmer",
+		description:
+			"In my first year of university I had my first contact with programming, since then I began to be interested in video game development. I have used Unity not only to create personal prototypes but also as a tool to tackle school projects (they were mostly interactive experiences). Additionally, I have gained experience working in multidisciplinary teams and have a basic understanding of visual and sound arts.",
+	},
+
+	about: {
+		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Unreal CTR Kart - Recreation",
+			description:
+				"A recreation of the kart mechanics in Crash Team Racing (PS1). My objective is to obtain the closest possible driving experience to the original title, implementing all the details into Unreal.",
+			logo: "../UE-Icon-2023-Black.svg",
+			linkText: "View Project",
+			link: "https://drive.google.com/file/d/1RVFflIneix0UrAnUZ1A61sq3UQWuLz79/view",
+		},
+
+		{
+			title: "Multiplayer Sumo Game - Prototype",
+			description:
+				"An android mobile game prototype that I've been working on during the quarantine. I like Brawl Starts‘s movement feeling, so I originally tried to imitate that, and then I came up with the idea of creating some kind of melee PvP.",
+			logo: "../U_Cube_1C_Black.svg",
+			linkText: "View Project",
+			link: "https://github.com/ianfranco98/Sumo-GamePrototype",
+		},
+
+		{
+			title: "Top-Down Bird Shooter - Prototype",
+			description:
+				"Prototype of a small 2D shooter where you are alone and you try to survive from waves of zombie birds.",
+			logo: "https://godotengine.org/assets/press/icon_monochrome_light.svg",//"https://godotengine.org/assets/press/icon_monochrome_dark.svg",
+			linkText: "View Project",
+			link: "https://drive.google.com/file/d/1L32ewOkeg5BuPDDX3378tndy-Ks1RztS/view?usp=drive_link",
+		},
+
+		{
+			title: "Stakeboard Game - Technical Test",
+			description:
+				"A prototype of a Skateboard Game. Development driven by my curiosity of this genre.",
+			logo: "../UE-Icon-2023-Black.svg",
+			linkText: "View Project",
+			link: "https://github.com/ianfranco98/SK-Prototype",
+		},
+
+		{
+			title: "Soulslike Game - Prototype",
+			description:
+				"A technical exploration of the common mechanics of soulslike games like dodge roll, hack and slash attacks with timed animations and basic melee enemies.",
+			logo: "../UE-Icon-2023-Black.svg",
+			linkText: "View Project",
+			link: "https://github.com/ianfranco98/Unreal-Soulslike-Prototype",
+		},
+
+		{
+			title: "My First Game - College Project",
+			description:
+				"Final project that i made for Multimedia Technology I in 2016. Basically it consists of programming a game using Processing and following a concrete theme and mechanics.",
+			logo: "../Processing.svg",
+			linkText: "View Project",
+			link: "https://github.com/ianfranco98/MyFirstGame",
+		},
+	],
+};
+
+export default INFO;
